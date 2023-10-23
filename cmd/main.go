bb33b30a9664ac84a2d43719f2a1d849c08ea7de
Matheus Batista <matheus.vbatista04@gmail.com>
2023-10-23 02:25:25 -0300

    feature: handle env and fire server

--- cmd/main.go
+++ cmd/main.go
@@ -1,14 +1,30 @@
 package main
 
 import (
+	"github.com/joho/godotenv"
 	"go-server/api"
+	"go-server/internal"
 	"log"
+	"os"
 )
 
 func main() {
 	log.Println("Setting up server...")
 
-	app := api.App{}
+	err := godotenv.Load(".env")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	app := api.App{
+		Provider: internal.Provider{
+			KeyId:   os.Getenv("KEY_ID"),
+			TeamId:  os.Getenv("TEAM_ID"),
+			KeyFile: "./auth-key.p8",
+			Topic:   os.Getenv("BUNDLE_ID"),
+			Client:  nil,
+		},
+	}
 
 	app.SetupServer()
 }
